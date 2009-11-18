2a1502984e7db313aab761ceed8b217f40f5d6ea
Robert Beekman <robertbeekman@noname.(none)>
2009-11-18 15:57:21 +0100

    added alert feature

--- justlogging.class.php
+++ justlogging.class.php
@@ -2,7 +2,6 @@
 class Justlogging {
   
   var $api_key;
-  var $host;
   var $log_key;
   
   function __construct($api_key, $log_key){
@@ -10,19 +9,25 @@ class Justlogging {
     $this->log_key = $log_key;
   }
   
-  function log($entry, $log_key = null){
+  
+  function alert($entry, $log_key = null){
+    $this->log($entry, $log_key, true);
+  }
+  
+  function log($entry, $log_key = null, $alert = false){
     $log_key = ($log_key) ? $log_key : $this->log_key;
 
-	// Check if cURL exists
-	if(!function_exists('curl_exec')){
-		throw new Exception('cURL is not installed, please install cURL.');
-	}
+  	// Check if cURL exists
+  	if(!function_exists('curl_exec')){
+  		throw new Exception('cURL is not installed, please install cURL.');
+  	}
     
-	// Prepare post data
+  	// Prepare post data
     $post_data = "access_key=".$this->api_key."&log_key=".$log_key."&entry=".$entry;
+    $post_data = ($alert == true) ? $post_data . "&alert=true" : $post_data;
 
     $ch = curl_init(); 
-    curl_setopt($ch, CURLOPT_URL, 'http://logs.justlogging.com'); // set url to post to 
+    curl_setopt($ch, CURLOPT_URL, 'http://logs.justlogging.com/log'); // set url to post to 
     curl_setopt($ch, CURLOPT_FAILONERROR, 1); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,1); // return into a variable 
     curl_setopt($ch, CURLOPT_TIMEOUT, 0); // times out after Ns 
@@ -30,15 +35,15 @@ class Justlogging {
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); // add POST fields 
     curl_setopt($ch, CURLOPT_VERBOSE, 0); // Shows debug info if set to 1
     $result = curl_exec($ch); // run the whole process 
-	$info = curl_getinfo($ch); // get header info
-	curl_close($ch); 
+    $info = curl_getinfo($ch); // get header info
+    curl_close($ch); 
     
-	// Justlogging returns a 201 if the entry has been saved, on all other codes, the saving failed.
-	if ($info['http_code'] != 201) {
-	  return false;
-	} else {
-	  return true;
-	}
+  	// Justlogging returns a 201 if the entry has been saved, on all other codes, the saving failed.
+  	if ($info['http_code'] != 201) {
+  	  return false;
+  	} else {
+  	  return true;
+  	}
   }
 }
 ?>
